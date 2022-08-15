688b4d0f34aa4a1ba887717c3f1162a90ddb1179
GonzaRossi10 <AD173819@slad.la.mcdcorp>
2022-08-15 20:53:09 -0300

    nueva actulizacion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express")
 const app = express()
 const path = require("path")
 
-app.use(express.static("public"))
+app.use(express.static("Public"))
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en puerto 3000") );
 
